fca9224d30d62aabdafa846cb8aaa1c5610d7442
Veter-ok <61391385+Veter-ok@users.noreply.github.com>
2022-11-19 13:23:15 +0300

    fixed bugs

--- Main.py
+++ Main.py
@@ -274,7 +274,6 @@ class DB_Controller():
 		self.connection = sqlite3.connect("bookkeppingDB.db")
 		self.login = login
 		self.cursor = self.connection.cursor()
-		self._addUser()
 
 	def _addUser(self):
 		self.cursor.execute("""INSERT INTO users(login, name) VALUES("lrd_yandex", "Родион")""")
